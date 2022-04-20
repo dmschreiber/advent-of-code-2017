cebf1033b4d08512689d71b93d87d2b2092df18a
Dan Schreiber <dmschreiber@gmail.com>
2022-04-20 04:29:50 -0500

    day 6 part 1

--- venv/all_test.py
+++ venv/all_test.py
@@ -1,6 +1,8 @@
 import unittest
 import day4
 import day5
+import day6
+
 
 class MyTestCase(unittest.TestCase):
     def test_day4(self):
@@ -13,6 +15,15 @@ class MyTestCase(unittest.TestCase):
         input = "./data/day5_test.txt"
         self.assertEqual(day5.part1(input),5)
         self.assertEqual(day5.part2(input),10)
+
+    def test_day6(self):
+        input = "0 2 7 0"
+        self.assertEqual(day6.redistribute(input),"2 4 1 2")
+        self.assertEqual(day6.redistribute("2 4 1 2"),"3 1 2 3")
+        self.assertEqual(day6.redistribute("3 1 2 3"),"0 2 3 4")
+        self.assertEqual(day6.redistribute("0 2 3 4"),"1 3 4 1")
+        self.assertEqual(day6.redistribute("1 3 4 1"),"2 4 1 2")
+        self.assertEqual(day6.part1(input),5)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ venv/day6.py
@@ -0,0 +1,50 @@
+import collections
+
+
+def redistribute(input):
+    banks = input.split()
+    banks = [int(i) for i in banks]
+
+    max_mem = max(banks)
+    which_bank = -1
+    for i in range(0,len(banks)):
+        if banks[i] == max_mem:
+            which_bank = i
+            break
+
+    if which_bank < 0:
+        raise NotImplemented
+
+    new_banks = banks.copy()
+    new_banks[which_bank] = 0
+
+    # 8 / 4 = 2
+    # 7 / 4 = 1.75
+    if banks[which_bank]/len(banks) != int(banks[which_bank]/len(banks)):
+        how_much_to_add = int(banks[which_bank]/len(banks)) + 1
+    else:
+        how_much_to_add = int(banks[which_bank]/len(banks))
+
+    count_down = banks[which_bank]
+    index = (which_bank + 1) % len(banks)
+    for i in range(0,len(banks)):
+        how_much_to_add = min(how_much_to_add,count_down)
+        new_banks[index] = new_banks[index] + how_much_to_add
+        count_down = count_down - how_much_to_add
+        index = (index + 1) % len(banks)
+
+    new_banks = [str(i) for i in new_banks]
+    return " ".join(new_banks)
+
+def part1(input):
+    history = []
+
+    loop_count = 0
+
+    current_state = input
+    while not current_state in history:
+        history.insert(0,current_state)
+        current_state = redistribute(current_state)
+        loop_count = loop_count + 1
+
+    return loop_count
